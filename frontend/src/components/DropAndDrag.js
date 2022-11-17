bba42094fdd69a7042848a73967f9a682b3ba6c7
HaiHart <hai.hohartfilli@hcmut.edu.vn>
2022-11-17 09:11:14 +0700

    add resizable

--- frontend/src/components/DropAndDrag.js
+++ frontend/src/components/DropAndDrag.js
@@ -4,6 +4,8 @@ import WaitZone from "./Wait";
 import DropZone from "./Drop";
 import Log from "./Log";
 
+// TODO: set some way to zoom only the Drop zone
+// also fine some way to be able to update dropzone size
 function DragDrop(){
 
     const [dat,setData] = useState({
@@ -12,6 +14,16 @@ function DragDrop(){
         Log: [],
     })
 
+    const [scale, setScale] = useState(1)
+
+    const ZoomWheel = (e)=>{
+        if (!e.shiftKey){
+            return
+        }
+        const delta = e.deltaY * -0.001
+        const newScale = delta + scale
+        setScale(newScale)
+    }
 
     if (dat.version<0){
         window.backend.Basic.Flip("yes", Number(0)).then(data=>{
@@ -50,30 +62,39 @@ function DragDrop(){
             <div>
                 ----------------------------------------------------
             </div>
-            <div style={{
-                border: '5rem solid yellow',
-                backgroundColor: 'yellow',
-                display: 'flex',
-                flex:8,
-                flexDirection: "row",
-                flexWrap: "wrap",
-                justifyContent: 'space-evenly',
+            <div onWheelCapture={ZoomWheel} style={{
+                height:'calc(100% - 40rem)',
+                overflowX:'scroll',
+                overflowY:'scroll',
             }}>
-                {
-                    [...Array(8)].map((x,i)=>{
-                        return(<DropZone items={dat.Rv} id={i} />)
-                    })
-                }
+                <div style={{
+                    border: '5rem solid yellow',
+                    backgroundColor: 'yellow',
+                    display: 'flex',
+                    flex:8,
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                    justifyContent: 'space-evenly',
+                    transformOrigin: "0 0",
+                    transform: `scale(${scale})`,
+                }}>
+                    {
+                        [...Array(8)].map((x,i)=>{
+                            return(<DropZone items={dat.Rv} id={i} />)
+                        })
+                    }
+                </div>
             </div>
-            </div>
-            <div style={{
-                width:'19%',
-                border: '0.3rem solid yellow',
-                height: '100%',
-                
-            }}>
-                <Log list={dat.Log}/>
-            </div>
+
+                </div>
+                <div style={{
+                    width:'19%',
+                    border: '0.3rem solid yellow',
+                    height: '100%',
+                    
+                }}>
+                    <Log list={dat.Log}/>
+                </div>
         </div>
     )
 }
